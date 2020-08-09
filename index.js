b589103b3719cf3f4f7e7d254691f5c3753349a4
blake-perkins-dev <blake_perkins@rocketmail.com>
2020-08-09 19:37:04 +0000

    correct files for launch

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
     
 app.get('/', function(req, res) {
-    // res.sendFile("index.html");
     res.sendFile("index.html");
 });
     
